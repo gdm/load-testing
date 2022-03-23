7d042ccddfcc3cbc45df76c20553ac1784c3546f
Dmytro Gorbunov <dmytro.gorbunov@gmail.com>
2022-03-23 22:54:16 +0100

    Remove some whitecharacters

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 'use strict';
 
 exports.handler = function( event, context, callback ) {
-    
     if( (event.name === 'Richard') || (event.name === 'rhyatt' ) ) {
-        
         return callback( null, { valid: true } );
     }
-    
+
     callback( new Error( 'unknown name' ) );
 };
--- test/index.test.js
+++ test/index.test.js
@@ -5,42 +5,30 @@ var expect = require( 'chai' ).expect;
 var myLambda = require( '../index' );
 
 describe( 'myLambda', function() {
-
     [
         "Richard",
         "rhyatt"
-
     ].forEach( function( validName ) {
 
         it( `successful invocation: name=${validName}`, function( done ) {
-
             var context = {
-
                 succeed: function( result ) {
-
                         expect( result.valid ).to.be.true;
                         done();
                     },
-
                 fail: function() {
-
                         done( new Error( 'never context.fail' ) );
                     }
             }
 
             myLambda.handler( { name: validName }, { /* context */ }, (err, result) => {
-            
                 try {
-                    
                     expect( err ).to.not.exist;
-                    
                     expect( result ).to.exist;
                     expect( result.valid ).to.be.true;
-                    
                     done();
                 }
                 catch( error ) {
-                
                     done( error );
                 }
             });
@@ -52,22 +40,15 @@ describe( 'myLambda', function() {
         undefined
 
     ].forEach( function( invalidName ) {
-
         it( `fail: when name is invalid: name=${invalidName}`, function( done ) {
-
             myLambda.handler( { name: invalidName }, { /* context */ }, (err,result) => {
-    
                 try {
-                    
                     expect( err ).to.exist;
                     expect( err.message ).to.equal( 'unknown name' );
-                    
                     expect( result ).to.not.exist;
-                    
                     done();
                 }
                 catch( error ) {
-                
                     done( eror );
                 }
             });
